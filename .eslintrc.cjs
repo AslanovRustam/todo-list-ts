9515485744fd0bebc603dc8ad9ddb47713e96019
Aslanov Rustam <aslanovrg@gmail.com>
2024-03-05 23:08:09 +0200

    add js lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "@wemake-services/javascript",
     "@wemake-services/typescript/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
